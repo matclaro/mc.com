d5b6ae177e812c6d089a247c6c12d7199caa6497
matclaro <info@matiasclaro.com>
2016-07-02 18:34:21 -0300

    mc.com
    estado 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	
+    $('#main-sidebar').simpleSidebar({
+     wrapper: '#main',
+     opener: '#toggle-sidebar',
+     animation: {
+      easing: 'easeOutQuint'
+     },
+     sidebar: {
+      align: 'right',
+      closingLinks: '.close-sb'
+     },
+     sbWrapper: {
+      display: false
+     }
+    });
+      
+    $(".tagline").letterfx({"fx":"fall","words":true,"timing":200});
+    $(".tagline-2").letterfx({"fx":"fall","words":true,"timing":300});
+
+});	
